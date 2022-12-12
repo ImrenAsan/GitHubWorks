79e34934d4cc2838dc2ac3f2acf31cb12819afd3
ImrenAsan <imrenasan@protonmail.com>
2022-12-12 11:17:36 +0100

    java odevler ikinci gün

---
+++ GitHubWorks/src/gun2odev1/Questin2.java
@@ -0,0 +1,17 @@
+package gun2odev1;
+
+public class Questin2 {
+
+	public static void main(String[] args) {
+		
+		// String ifadeyi kullanicidan alma ve konsolda yazdirma
+		
+		String myFirstName= "Imren", myLastName= "Asan", myAddress= "Hille - Germany";
+		
+		System.out.println("My name's " + myFirstName+ " " + myLastName);
+		System.out.println("I live in " + myAddress);
+		
+
+	}
+
+}
---
+++ GitHubWorks/src/gun2odev1/Question5.java
@@ -0,0 +1,27 @@
+package gun2odev1;
+
+import java.util.Scanner;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		
+		// Karenin alan ve cevresi hesaplama
+		
+		Scanner scan = new Scanner (System.in);
+		
+		System.out.println("Karenin 1 kenar uzunlugunu giriniz: ");
+		
+		int kenar = scan.nextInt();
+		int cevre = kenar * 4 ;
+		int alan = kenar * kenar;
+		
+		System.out.println("Karenin cevre uzunlugu,  " + cevre + " birimdir.");
+		System.out.println("Karenin alani, " + alan + " birimdir.");
+		
+		
+		
+
+	}
+
+}
